1740fa6063fecf1fb5e38ea28a38342c3e7941b3
myobie <me@nathanherald.com>
2015-02-17 11:30:20 +0100

    Forgot and left some putss in there

--- spec/mashed/stringy_hash_spec.rb
+++ spec/mashed/stringy_hash_spec.rb
@@ -23,7 +23,6 @@ describe Mashed::StringyHash do
     before { s[:a] = "A" }
     it { expect(s[:a]).to eq("A") }
     it { expect(s["a"]).to eq("A") }
-    it { puts s.inspect; puts s.class; puts s.keys.inspect; puts s.key?("a").inspect }
     it { expect(s.key?(:a)).to be(true) }
     it { expect(s.key?("a")).to be(true) }
   end
